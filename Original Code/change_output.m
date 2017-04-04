25ed2f778a380172ad2d44266e056d4203b04b73
Susan Meerdink <susan.meerdink@gmail.com>
2017-04-04 13:25:00 -0700

    Compressed old code files

--- Original Code/change_output.m
+++
@@ -1,7 +0,0 @@
-% run this script after 'irFileOpen' function.
-% This script is to specify some variable that are used in function "integer2temp".
-
-global fileInfo
-fileInfo.data=fileInfo.overlayDefaultThermal; % TIR data is replace with the overlaping portion of TIR data.
-fileInfo.width=160;
-fileInfo.height=120;
